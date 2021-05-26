55d44064a69c2bc25191622b524275ba382ebf5e
ahmedragab20 <ahmedragab20901@gmail.com>
2021-05-26 11:11:40 +0200

    create project

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.links');
+const links = document.querySelectorAll('.links li');
+const toTop = document.querySelector('.to-top');
+
+burger.addEventListener("click", ()=>{
+    navLinks.classList.toggle('active');
+//the Links Fade
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+//the X transition
+    burger.classList.toggle('toggle');
+});
+
+// The Iphone Slider
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    loop:true,
+  });
+
+//to top button
+
+window.addEventListener("scroll", ()=>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add('go');
+    }else{
+        toTop.classList.remove('go');
+    }
+})
